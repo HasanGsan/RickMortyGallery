63140401dbfb2461abd482128ee0dbe6d77bd8f9
HasanSan <145437224+HasyanGsan@users.noreply.github.com>
2024-08-05 22:41:31 +0300

    Готовая рабочая навигация.

---
+++ app/src/main/java/com/example/galleryapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.galleryapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.galleryapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_bar)
+        val controller = findNavController(R.id.fragmentContainerView)
+        btnNavView.setupWithNavController(controller)
+
+
+
+
+    }
+
+
+
+}
